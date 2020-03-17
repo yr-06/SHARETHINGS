efcbeb2ae7c473f8e454c59f1ab324ae58052107
Yael Ruimy <yael@LAPTOP-8HA124E3.localdomain>
2020-03-17 14:21:30 +0100

    fonction pour changer les données

---
+++ chiffrement.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <crypt.h>
+#include <string.h>
+
+
+char* chiffrementMdp(char* chaine)
--- user.c
+++ user.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-//structure pour compte users. si compte commun , mettre un int qui dit si oui ou non on est admin.
+//structure pour compte users. Si compte commun , mettre un int qui dit si oui ou non on est admin.
 typedef struct compte_u{
         char nom[32];
         char prenom[32]
@@ -54,7 +54,33 @@ void set_ID(user u, char * ID){
         strcpy(u->ID, ID);
 
 }
+//fonctions : 
+//1) permet de changer de donnees (mdp, ID , etats des ressources...)
+user changer_donnees(user u, int choix){
+	char newMdp[32];
+	char checkNewMdp[32];
+	char newID[64];
+	printf("Pour changer de mot de passe, tapez 1\n");
+	printf("Pour changer d'identifiant, tapez 2\n");
+	scanf("%d", &choix);
+	switch(choix){
+		case 1:
+			printf("Vous avez choisi de changer de mot de passe.\n");
+			printf("Entrez votre nouveau mot de passe: \n");
+			scanf("%s",&newMdp);
+			printf("Veuillez confirmer votre mot de passe: \n");
+			scanf("%s",&check_newMdp);
+			if(strcmp(newMdp,check_newMdp) == 0){ //strcmp :permet de comparer deux chaînes de caractères et de savoir si la première est inférieure, égale ou supérieure à la seconde.
+				printf("Votre message a été changé avec succès !\n");
+			}else{
+				printf("Erreur. Recommencez !\n");
+				return changer_donnees(user u, int choix);
+			}
+		case 2:
+			printf("Vous avez choisi de changer d'identifiant."
+			printf("Entrez votre nouvel identifiant: \n");
+			scanf("%s",newID);
+	}
 
-user changer_mdp(user u){
-
+}
 
